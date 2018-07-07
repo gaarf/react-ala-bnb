38e95956f1dbb880d34404b0f351db1a919b2ec3
gaarf <onlineallthetime@gmail.com>
2018-07-07 14:29:20 -0700

    rename Routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import Hello from '../containers/Hello';
 
-export default function App() {
+export default function Routes() {
   return (
     <Router>
       <Route exact path="/" component={Hello} />
