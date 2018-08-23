d1e8aac2a70912b684d0ecfa9a68fce43c2bbf07
Adrien Cahen <adrien.cahen@airbnb.com>
2018-08-23 11:54:04 -0700

    async await and hoc sit in a boat hoc falls in the water who remains?

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,12 +3,13 @@ import '../layout.css';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../store';
-import HelloContainer from './Hello';
+// import HelloContainer from './Hello';
+import TimeIsAnIllusion from './TimeIsAnIllusion';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HelloContainer />
+      <TimeIsAnIllusion />
     </Provider>
   );
 }
